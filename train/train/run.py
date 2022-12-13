fe47b442ed75ac59bab088007c7ebc7b0213abd4
Enzo Fragale <fragale.enzo@gmail.com>
2022-12-13 14:07:36 +0100

    completed run.py code at end of lab

--- train/train/run.py
+++ train/train/run.py
@@ -24,6 +24,7 @@ def train(dataset_path, train_conf, model_path, add_timestamp):
                 dense_dim: 64
                 min_samples_per_label: 10
                 verbose: 1
+                train_ratio: 0.8
             }
     :param model_path: path to folder where training artefacts will be persisted
     :param add_timestamp: boolean to create artefacts in a sub folder with name equal to execution timestamp
@@ -37,7 +38,9 @@ def train(dataset_path, train_conf, model_path, add_timestamp):
     
     # instantiate a LocalTextCategorizationDataset, use embed method from preprocessing module for preprocess_text param
     # use train_conf for other needed params
-    dataset = LocalTextCategorizationDataset("../../stackoverflow_posts.csv", 100)
+    # ../../stackoverflow_posts.csv
+    dataset = LocalTextCategorizationDataset(filename = dataset_path, batch_size = train_conf["batch_size"],
+                 train_ratio = train_conf["train_ratio"], min_samples_per_label = train_conf["min_samples_per_label"])
 
     logger.info(dataset)
 
@@ -45,29 +48,29 @@ def train(dataset_path, train_conf, model_path, add_timestamp):
     # add a dense layer with relu activation
     # add an output layer (multiclass classification problem)
     model = Sequential()
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(train_conf["dense_dim"], activation='relu'))
     model.add(Dense(dataset.get_num_labels, activation='softmax'))
 
     # model fit using data sequences
-    train_history = 
+    train_history = model.fit()
     
 
     # scores
-    scores = model.evaluate_generator(dataset.get_test_sequence(), verbose=0)
+    scores = model.evaluate_generator(dataset.get_test_sequence(), verbose=train_conf["verbose"])
 
     logger.info("Test Accuracy: {:.2f}".format(scores[1] * 100))
 
-    # TODO: CODE HERE
     # create folder artefacts_path
+    os.mkdir(artefacts_path)
 
-    # TODO: CODE HERE
     # save model in artefacts folder, name model.h5
+    model.save(os.path.join(artefacts_path, "model.h5"))
 
-    # TODO: CODE HERE
     # save train_conf used in artefacts_path/params.json
+    json.dump(train_conf, os.path.join(artefacts_path, "params.json"))
 
-    # TODO: CODE HERE
     # save labels index in artefacts_path/labels_index.json
+    json.dump(dataset.get_label_to_index_map(), os.path.join(artefacts_path, "labels_index.json"))
 
     # train_history.history is not JSON-serializable because it contains numpy arrays
     serializable_hist = {k: [float(e) for e in v] for k, v in train_history.history.items()}
